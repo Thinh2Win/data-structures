71e59cd56ad13e4983fb6efa6b5a4eafecbaf6bb
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-02-24 16:05:23 -0800

    Finished functional Queue Functional pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,15 +5,30 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
-
+  var n = 0;
   someInstance.enqueue = function(value) {
+    // Add a string to the back of the queue
+    if (storage[n] === undefined) {
+      storage[n] = value;
+      n++;
+    }
   };
-
+  var d = 0;
   someInstance.dequeue = function() {
+    // Remove and return the string at the front of the queue
+    var holder = storage[d];
+    delete storage[d];
+    d++;
+    return holder;
   };
 
   someInstance.size = function() {
+    // should return the number of items in the queue
+    return _.allKeys(storage).length;
   };
-
+  console.log(someInstance);
   return someInstance;
 };
+
+// understanding the assignment
+// Queue is going to be our class function that creates multiple instances with similar elements
